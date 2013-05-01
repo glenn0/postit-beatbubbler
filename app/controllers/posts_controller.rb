42a22624cb45116cdf0bf7ee6ee9dd9180eacdbc
glenn <glenn@G-Dog.local>
2013-05-01 16:00:16 +1000

    Conditional (logged_in?) edit for Post added.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
 
   before_filter :require_user, only: [:new, :create]
+  before_filter :find_post, only: [:edit, :update] #creates the @post variable for the require_creator filter.
+  before_filter :require_creator, only: [:edit, :update]
 
   def index
     @posts = Post.all
@@ -24,6 +26,24 @@ class PostsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+# Filter Methods
+  
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def require_creator
+    unless logged_in? && current_user == @post.user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
     end
   end
 
